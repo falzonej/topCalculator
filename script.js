1ed314cc89e1abdc333943f2a1d06dffde9f5a02
JoeFalzone <falzonej@gmail.com>
2022-05-17 20:18:49 -0400

    stored operators in object and converted to => funcs

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const add = function(x, y) {
-	let result = x + y;
-  return result
-};
+const operators = {
 
-const subtract = function(x, y) {
-  let result = x - y;
-  return result;
-};
-
-const sum = function(array) {
-	let result = 0;
-  for (i = 0; i < array.length; i++){
-    result += array[i];
+  add: (x, y) => {
+  	let result = x + y;
+    return result
+  },
+  subtract: (x, y) => {
+    let result = x - y;
+    return result;
+  },
+  //mind the 2 below funcs as they only take arrays
+  sum: (array) => {
+  	let result = 0;
+    for (i = 0; i < array.length; i++){
+      result += array[i];
+    }
+    return result;
+  },
+  multiply: (array) => {
+  	let result = 1;
+    for (i = 0; i < array.length; i++){
+      result *= array[i];
+    }
+    return result;
   }
-  return result;
-};
-
-const multiply = function(array) {
-	let result = 1;
-  for (i = 0; i < array.length; i++){
-    result *= array[i];
-  }
-  return result;
-};
+}
