7db3efe9a68d9c50f83d0e7c8388586f9460be9f
JoeFalzone <falzonej@gmail.com>
2022-05-18 17:09:41 -0400

    added selectors to js for html buttons

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const calculatorDisplay = document.querySelector('#calculatorDisplay');
-
+const button1 = document.querySelector('#one');
+const button2 = document.querySelector('#two');
+const button3 = document.querySelector('#three');
+const button4 = document.querySelector('#four');
+const button5 = document.querySelector('#five');
+const button6 = document.querySelector('#six');
+const button7 = document.querySelector('#seven');
+const button8 = document.querySelector('#eight');
+const button9 = document.querySelector('#nine');
+const button0 = document.querySelector('#zero');
+const addOperator = document.querySelector('#addOperator');
+const subtractOperator = document.querySelector('#subtractOperator');
+const multiplyOperator = document.querySelector('#multiplyOperator');
+const divideOperator = document.querySelector('#divideOperator');
+const clearOperator = document.querySelector('#clear');
+const equalsOperator = document.querySelector('#equalsOperator');
 
 const operators = {
   add: (x, y) => {
